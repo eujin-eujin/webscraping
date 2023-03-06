8abda658f6a8d41e6b37a866ef115abb7d5d2ab8
Eujinty Rathna <92358653+eujin-eujin@users.noreply.github.com>
2023-03-06 09:11:48 +0530

    AmazonProducts.py
    the python file to crawl the amazon website and scrape the product details

--- AmazonProducts.py
+++ AmazonProducts.py
@@ -1,46 +1,55 @@
- # Importing required libraries
+# Importing required libraries
 
 import pandas as pd
 import re
 import requests as req
 from bs4 import BeautifulSoup as bs
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from time import sleep
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 import random
 
 
 class Amazon():
     
+    ''' this class is to scrape the amazon product 
+    data like product name , price , title, ...'''
+   
     data_list = [] # Class variable to store the scraped data
     sleep_time = random.uniform(1,5)
+    
     # Class constructor to initialize instance variables    
-    def __init__(self,url,header,driver,query):
+    def __init__(self,url,header,query):
         self.url = url
         self.header = header
-        self.driver = driver
         self.query = query
         self.df = None
-    
-    # Method to get HTML content of the Amazon search results page
+        self.session = req.Session()
+     
+   #thtis function just returns the html response
     def get_html(self):
-        page = self.driver.get(self.url)
-        sleep(3)
-        search_bar = self.driver.find_element(By.XPATH,'//*[@id="twotabsearchtextbox"]')
-        search_bar.send_keys(self.query[0])
-        search_bar.send_keys(Keys.RETURN)
-        html = self.driver.page_source
+        self.session.headers.update(self.header)
+        html = self.session.get(self.make_url())
         sleep(self.sleep_time)
-        self.driver.close()
         return html 
     
-    # Method to scrape data from the HTML content of a search results page
+    # this function joins the relative links to domain and returns main as links
+    def make_url(self):
+        split_query = self.query.split() # spliting each word in the search query
+        url_ = self.url+'/s?k='
+        for query in split_query: #concatenate the words with domain
+            url_ = url_+query+'+'
+        url_= url_[:-1]
+        print(url_)
+        return url_
+    
+    # Method to scrape the product details of the search results page
     def get_product_data(self,html):
-        soup = bs(html,'lxml')
-        products = soup.find_all('div',attrs={'data-asin':re.compile(r'[A-Z0-9]{10}')})
+        
+        soup = bs(html.text,'lxml')
+        products = soup.find_all('div',attrs={
+        'data-asin':re.compile(r'\w{10}'),
+        'data-index':re.compile(r'\d+')
+        }) # to avoid the recommendation products get matched the parsing we are 
+        #we are specificlly getting the searched result product alone
         
         for product in products:
             try:
@@ -53,7 +62,7 @@ class Amazon():
             except : 
                 product_url = ''
             try:
-                title = product.find('span',class_='a-size-medium a-color-base a-text-normal').text.strip()
+                title = product.h2.text.strip()
             except: 
                 title = ''
             try :    
@@ -66,16 +75,17 @@ class Amazon():
                 price = ''
                 
             try: 
-                out_of_5 = product.find('span',class_='a-icon-alt').text.strip()
+                out_of_5 = product.find('span',attrs={'aria-label':re.compile('out of 5')}).i.text.strip()
             except: 
                 out_of_5 = ''
             try: 
-                total_ratings = product.find('span',class_='a-size-base s-underline-text').text.strip()[1:-1]
+                total_ratings = product.find('span',attrs={'aria-label':re.compile(r'^\d{1,3}(,\d{2,3})*$')}).text.strip()[1:-1]
             except:
                 total_ratings = ''
+                
             data = {
             'product url':product_url,
-            'asin':asin_no,
+            'ASIN':asin_no,
             'title' : title,
             'price' : price,
             'total ratings':total_ratings,
@@ -88,20 +98,23 @@ class Amazon():
         #pagination
         next_page = soup.find('a',attrs={'class':'s-pagination-item s-pagination-next s-pagination-button s-pagination-separator'})
         if next_page is not None:
-            html_ = req.get(self.url+next_page['href'],headers=self.header)
+            session = req.Session()
+            
+            html = self.session.get(self.url+next_page['href'])
             sleep(self.sleep_time)
-            self.get_product_data(html_.text)
+            self.get_product_data(html)
+            
             
     # Method to scrape data from all the search results pages        
     def scrape_data(self):
         html = self.get_html()
         self.get_product_data(html)
         
+        
     # Method to save the scraped data to a CSV file    
     def save(self):
         df = pd.DataFrame(self.data_list)
-        #csv = df.to_csv('digital_products.csv',index=False)
-        print(df)
+        csv = df.to_csv('digital_products.csv',index=False)
         self.df = df
     
 
@@ -112,23 +125,18 @@ if __name__=='__main__':
     HEADER = {
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
 }
-
-    # Create a ChromeOptions object
-    chrome_options = Options()
-
-    # set up Chrome options
-
-    chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36')
-    chrome_options.add_argument('--headless') 
-
-    # set up Chrome service and driver
-    chrome_service = Service(executable_path='ex:user/name/downloads/chromedriver') # use your chrome driver path here
-    chrome_service.start()
-    DRIVER = webdriver.Chrome(service=chrome_service, options=chrome_options)
     
+    queries= ['smart hand band', 'smart watch', 'smart tag', 'wireless earbuds', 'bluetooth headphones',
+              'portable speaker', 'streaming device', 'external hard drive', 'gaming laptop', 'gaming mouse', 
+              'gaming keyboard', 'smartphone gimbal', 'action camera', 'smart doorbell', 'smart thermostat',
+              'smart plug', 'smart light bulbs', 'robot vacuum cleaner', 'air purifier', 'smart water bottle']
     
-    query = ['camera']
-    amazon = Amazon(URL,HEADER,DRIVER,query)
-    amazon.scrape_data()
+    for query in queries : 
+        amazon = Amazon(URL,HEADER,query) # Amazon class instance 
+        amazon.scrape_data() # this method starts crawling
+        sleep(amazon.sleep_time)
+        
+    #this saves the file as csv        
     amazon.save()
+
     
